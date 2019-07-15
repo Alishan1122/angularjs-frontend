1a59f6e405072ea71c338f32ae92c6cc554fba5f
Ali Shan Nawaz <AliShan.Nawaz@outlook.com>
2019-07-15 21:03:35 +0200

    front-end task first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import {HomeComponent} from './pages/home/home.component';  
+import {AboutComponent} from './pages/about/about.component';  
+import {ContactComponent} from './pages/contact/contact.component'; 
+
+const routes: Routes = [  
+{path: '',component: HomeComponent},  
+{path: 'about',component: AboutComponent},  
+{path: 'contact',component: ContactComponent},  
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
